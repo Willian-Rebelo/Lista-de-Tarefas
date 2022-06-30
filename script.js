159d4170c21b847bcdea8edd4407f976365795f6
Willian-Rebelo <107209823+Willian-Rebelo@users.noreply.github.com>
2022-06-30 16:13:28 -0300

    .

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // estado de aplicação
-const tarefa = ['Estudar HTML', 'Estudar CSS', 'Estudar JS']
+const tarefas = []
 
+// alteradores de estado da aplicação 
 function add() {
     const input = document.querySelector("input")
     const tarefa = input.value
@@ -8,17 +9,27 @@ function add() {
     input.value = "" 
     render()
 }
-
-    function render( ) {
+  // mostrar estado na tela 
+    function render() {
     const ul = document.querySelector("ul")
-
+    ul.innerHTML = null
+    
     tarefas.forEach(function (tarefa) {
       const li = document.createElement("li")
       li.innerText = tarefa
-      ul.appendChild(li).
+      ul.appendChild(li)
+      const button=document.createElement("button")
+      button.innerHTML= "X"
+      button.addEventListener("click" ,function(){
+        const index= tarefas.indexOf(tarefa)
+        tarefas.splice(index, 1)
+        render()
+      })
+      li.appendChild (button)
     })
     
     }
+    // iniciar 
     render()
 
 
